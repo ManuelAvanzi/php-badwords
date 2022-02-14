c59e578bd12541e63bb7e1be828247c423a322d1
ManuelAvanzi <manuel.92.avanzi@gmail.com>
2022-02-14 23:22:13 +0100

    finish

--- index.php
+++ index.php
@@ -3,12 +3,21 @@
   $testo="1. Un robot non può recar danno a un essere umano né può permettere che, a causa del proprio mancato intervento, un essere umano riceva danno.
           2. Un robot deve obbedire agli ordini impartiti dagli esseri umani, purché tali ordini non contravvengano alla Prima Legge.
           3. Un robot deve proteggere la propria esistenza, purché questa autodifesa non contrasti con la Prima e con la Seconda Legge.";
+  #parola da censurare nel Url
   $badword=$_GET["badword"];
+  $replace="***";
   
   # misuro la lunghezza della stringa con strlen
   $size=strlen($testo);
+
+  #cerco la badword nella stringa
+  //var_dump(strpos($testo,$badword));
+
+  #sostituiso la badword trovata con "***"
+  $testo_censurato=str_replace($_GET["badword"], $replace, $testo);
   
 
+  
 ?>
 
 <!DOCTYPE html>
@@ -20,16 +29,22 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>TRE LEGGI DELLA ROBOTICA</h1>
      <div>
-         <h3>
-             <?php  echo $testo?>
-         </h3>
-         <h3>
-             <br><br>
+         <?php  echo $testo?>
+         <br><br>
 
+
+         <h3>
+             lunghezza del testo:
              <?php echo $size?> 
          </h3>
+     </div>
+
+     <br><br>
+     <div>
+         <!--stampo il testo censurato-->
+         <?php echo $testo_censurato ?>
      </div>
 
 
